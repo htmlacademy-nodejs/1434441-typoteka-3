a7942de87d219c23ba8e25077275c5b70d20c483
Ivan Lomov <ilomov@ivi.ru>
2022-04-15 20:07:48 +0300

    refs module3-task2 добавит логики промежуточной WIP

--- src/express/express.js
+++ src/express/express.js
@@ -5,11 +5,16 @@ const personalRoutes = require(`./routes/personal`);
 const articleRouter = require(`./routes/article`);
 
 const app = express();
-
 const port = 8080;
+app.listen(port);
+
+app.set(`views`, __dirname + `/templates`);
+app.set(`view engine`, `pug`);
+
+app.use(express.static(__dirname + `/public`));
 
 app.get(`/`, (req, res) => {
-  res.send(`${req.originalUrl}`);
+  res.render(`main`);
 });
 app.get(`/register`, (req, res) => {
   res.send(`${req.originalUrl}`);
@@ -23,5 +28,3 @@ app.get(`/search`, (req, res) => {
 
 app.use(`/my`, personalRoutes);
 app.use(`/article`, articleRouter);
-
-app.listen(port);

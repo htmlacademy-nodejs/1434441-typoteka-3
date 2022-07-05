994d8ac6da7a53bbd2b0ab7cc1d00f82d9570883
Ivan Lomov <ilomov@ivi.ru>
2022-07-05 17:45:04 +0300

    refs module4-task1 добавит константных значений

--- src/service/constants.js
+++ src/service/constants.js
@@ -1,5 +1,8 @@
 'use strict';
 
+module.exports.OFFER_MAX_COUNT = 1000;
+module.exports.MAX_ID_LENGTH = 6;
+
 module.exports.DEFAULT_COMMAND = `--help`;
 module.exports.USER_ARGV_INDEX = 2;
 module.exports.ExitCode = {
@@ -7,8 +10,13 @@ module.exports.ExitCode = {
   success: 0,
 };
 
+module.exports.API_PREFIX = `/api`;
+
 module.exports.HttpCode = {
   OK: 200,
+  CREATED: 201,
+  DELETED: 204,
+  BAD_REQUEST: 400,
   UNAUTHORIZED: 401,
   FORBIDDEN: 403,
   NOT_FOUND: 404,

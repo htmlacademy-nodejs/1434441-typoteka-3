34192969f45322ca07c29273e846bdcbf1d0c6c0
Ivan Lomov <ilomov@ivi.ru>
2022-07-05 17:46:57 +0300

    refs module4-task1 добавит классы с методами для работы с данными

---
+++ src/service/data-service/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const ArticlesService = require(`./articlesService`);
+const CategoriesService = require(`./categoriesService`);
+const CommentsService = require(`./commentsService`);
+const SearchService = require(`./searchService`);
+
+module.exports = {
+  ArticlesService,
+  CategoriesService,
+  CommentsService,
+  SearchService
+};

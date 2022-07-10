328b05fd647f98f22cc8024d2e6110189407ce8e
Ivan Lomov <ilomov@ivi.ru>
2022-07-11 00:31:29 +0300

    refs module4-task2 добавит тесты и обновит код сервера

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -9,9 +9,10 @@ const getMockData = require(`../lib/get-mock-data`);
 const {HttpCode, API_PREFIX} = require(`../constants`);
 const routes = require(`../api`);
 
-const chalk = require(`chalk`);
+const {getLogger} = require(`../lib/logger`);
 
 const notFoundMessageText = `Not Found`;
+const logger = getLogger({name: `api`});
 
 app.use(express.json());
 app.use(API_PREFIX, routes);
@@ -32,22 +33,35 @@ module.exports = {
         }
         res.send(data);
       })
-        .catch(() =>
-          res.send(notFoundMessageText));
+        .catch((err) =>
+          logger.error(`An error occurred: ${err.message}`)
+        );
     });
 
+    app.use((req, res, next) => {
+      logger.debug(`Request on route ${req.url}`);
+      res.on(`finish`, () => {
+        logger.info(`Response status code ${res.statusCode}`);
+      });
+      next();
+    });
+
+    app.use((err, _req, _res, _next) => {
+      logger.error(`An error occurred on processing request: ${err.message}`);
+    });
 
     app.use((req, res) => {
       res.status(HttpCode.NOT_FOUND).send(notFoundMessageText);
+      logger.error(`Route not found: ${req.url}`);
     });
 
     app
       .listen(port)
       .on(`listening`, () => {
-        console.info(chalk.green(`Ожидаю соединений на ${port}`));
+        logger.info(`Listening to connections on ${port}`);
       })
       .on(`error`, ({message}) => {
-        console.error(chalk.red(`Ошибка создания сервера: ${message}`));
+        logger.error(`An error occurred on server creation: ${message}`);
       });
   }
 };
